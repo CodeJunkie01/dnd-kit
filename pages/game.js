81e53f41e551857f9e987c76c5d18ee1e009db38
CodeJunkie01 <p2h4go@gmail.com>
2022-07-08 19:27:38 +0200

    added daisy ui config and header

--- pages/game.js
+++ pages/game.js
@@ -1,9 +1,11 @@
 import PlayingField from "../components/PlayingField";
 import { Container } from "@mui/system";
-
+import Header from "../components/Header/Header";
 function Game() {
   return (
     <>
+<Header></Header>
+
       <PlayingField></PlayingField>
       <br></br>
       <br></br>
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+  content: ['./components/**/*.js','./pages/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}
